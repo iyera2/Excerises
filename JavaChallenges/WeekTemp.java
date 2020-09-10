f98fbb58da4dc7909f1c3732a6aadd135b89d4f8
Arjun Iyer <iyera2@s.dcsdk12.org>
2020-09-10 11:25:55 -0600

    Fix Later

---
+++ JavaChallenges/WeekTemp.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class WeekTemp {
+    public static void main(String[] args) {
+        System.out.println("Enter Weekly Temp. for Sunday ");
+        Scanner scanner = new Scanner(System.in);
+        // Integer temperature = scanner.nextInt();
+        System.out.println("Enter Weekly Temp. for Monday ");
+        int try1 = scan.nextInt();
+        System.out.println("Enter Weekly Temp. for Tuesday ");
+        scanner = Scanner(System.in);
+        System.out.println("Enter Weekly Temp. for Wednesday ");
+        scanner = Scanner(System.in);
+        System.out.println("Enter Weekly Temp. for Thursday");
+        scanner = Scanner(System.in);
+        System.out.println("Enter Weekly Temp. for Friday");
+        scanner = new Scanner(System.in);
+        System.out.println("Enter Weekly Temp. for Saturday");
+        scanner = new Scanner(System.in);
+        // System.out.println("Sunday Temperature is " +  Integer.toString(temperature));
+        
+    }
+}
