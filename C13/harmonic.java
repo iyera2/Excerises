b386099611c76223daa171b4781b66b80d0394c7
Arjun Iyer <iyera2@s.dcsdk12.org>
2020-10-25 17:10:37 -0600

    Adding new files

--- C13/harmonic.java
+++ C13/harmonic.java
@@ -2,14 +2,17 @@ import java.util.Scanner;
 
 public class harmonic{
     public static void main(String[] args) {
+
+        //Resistance to change
+
         double x, y;
         Scanner scan = new Scanner( System. in);
 
         System.out.println("Enter X:");
-        x = scan.nextDouble();
+        x = scan.nextInt();
 
         System.out.println("Enter Y:");
-        y = scan.nextDouble();
+        y = scan.nextInt();
 
         Double arthmetic = (x+y)/2;
         Double harmonic = 2/(1/x + 1/y);
---
+++ JavaChallenges/Color.java
@@ -0,0 +1,11 @@
+import java.lang.Math;
+import java.util.Random;
+
+
+public class Color{
+    Random ran = new Random();
+   public void getred(){
+       
+   }
+}
+
