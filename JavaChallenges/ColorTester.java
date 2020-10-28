e9640f339c3630bf1d98b51d850d2ae9815b9382
Arjun Iyer <iyera2@s.dcsdk12.org>
2020-10-28 10:58:26 -0600

    Adding random

--- JavaChallenges/ColorTester.java
+++ JavaChallenges/ColorTester.java
@@ -2,11 +2,16 @@ public class ColorTester {
 
 	public static void main(String[] args) {
         Color new_color = new Color(161, 131, 131);
+        Color randomColor = new Color();
 
         System.out.println(new_color.willDisplay());
+        System.out.println(randomColor.willDisplay());
 
         new_color.BrightenColor(60);
         new_color.DarkenColor(20);
+
+        System.out.println(new_color.willDisplay());
+        System.out.println(randomColor.willDisplay());
     }
 
 }
