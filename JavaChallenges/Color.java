e88fbb17829bba6d300d9fac94862d0e49ff2381
Arjun Iyer <iyera2@s.dcsdk12.org>
2020-10-28 10:43:05 -0600

    Adding Color

--- JavaChallenges/Color.java
+++ JavaChallenges/Color.java
@@ -1,11 +1,73 @@
-import java.lang.Math;
 import java.util.Random;
 
+public class Color {
 
-public class Color{
-    Random ran = new Random();
-   public void getred(){
-       
-   }
+	private int red;
+	private int green;
+	private int blue;
+	Random rand = new Random();
+
+	public Color() {
+
+		red = rand.nextInt( 256 );
+		green = rand.nextInt( 256 );
+		blue = rand.nextInt( 256 );
+
+	}
+
+	public Color( int red, int green, int blue ) {
+
+		this.red = red;
+		this.green = green;
+		this.blue = blue;
+
+    }
+    
+	public void setRed( int red ) { 
+		this.red = red; 
+		finalColor();
+	}
+	public void setRed() { 
+        red = rand.nextInt( 256 ); 
+    }
+
+	public void setGreen( int green ) { 
+		this.green = green; 
+		finalColor();
+	}
+	public void setGreen() { 
+        green = rand.nextInt( 256 ); 
+    }
+
+	public void setBlue( int blue ) { 
+		this.blue = blue;
+        finalColor();
+    }
+
+    public String willDisplay() {
+        return "[" + red + "," + green + "," + blue + "]";
+    }
+        
+    private void finalColor() {
+            if( red > 255 ) {
+                red = 255;
+            }
+            if( red < 0 ) {
+                red = 0;
+            }
+            if( green > 255 ) {
+                green = 255;
+            }
+            if( green < 0 ) {
+                red = 0;
+            }
+            if( blue > 255 ) {
+                blue = 255;
+            }
+            if( blue < 0 ) {
+                blue = 0; 
+            }
+
+        
+    }
 }
-
---
+++ JavaChallenges/ColorTester.java
@@ -0,0 +1,9 @@
+public class ColorTester {
+
+	public static void main(String[] args) {
+        Color new_red = new Color(161, 131, 131);
+
+        System.out.println(new_red.willDisplay());
+    }
+
+}
