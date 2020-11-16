53245d92af32fb159712233b313cce85ea1558bf
Arjun Iyer <iyera2@s.dcsdk12.org>
2020-11-16 13:49:19 -0700

    Pushing files

--- JavaChallenges/LunchAccountTester.java
+++ JavaChallenges/LunchAccountTester.java
@@ -1,14 +1,23 @@
 public class LunchAccountTester{ // getting constructor error don't know why
     public static void main(String[] args) {
 
-        LunchAccount firststudent = new LunchAccount();
-        LunchAccount secondstudent = new LunchAccount(100.0);
-
+        LunchAccount firststudent = new LunchAccount(10000.0);
+        System.out.println(student1.getBalance());
+        firststudent.addBalance(10);
+        System.out.println(student1.getBalance());
+        testPurchase(student1.buyLunch());
         System.out.println(firststudent);
+
+
+        LunchAccount secondstudent = new LunchAccount(100.0);
+        System.out.println(student2.getBalance());
+        firststudent.addBalance(100);
+        System.out.println(secondstudent.getBalance());
+        testPurchase(student1.buyLunch(-100));
         System.out.println(secondstudent);
 
-        firststudent.addBalance(10);
-        secondstudent.addBalance(0);
+        
+        
 
         System.out.println(firststudent);
         System.out.println(secondstudent);
