b92d9d72369daefe0e8ddf42a4685ade3c73dfee
Arjun Iyer <iyera2@s.dcsdk12.org>
2020-12-11 10:33:32 -0700

    Initial OOP commit

--- JavaChallenges/Todo/todo2.java
+++ JavaChallenges/Todo/todo2.java
@@ -5,14 +5,6 @@ public class todo2{
 
     public static void main(String[] args) {
         File file = new File("");
-        Scanner scan = new Scanner(file);
-        Scanner newScan = new Scanner(System.in);
-
-
-        try{
-            PrintWriter output = new PrintWriter(new FileWriter("", true));
-
-            System.out.println("Menu: ");
-        }
+        PrinterWriter outpit, 
     }
 }
